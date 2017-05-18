94c8e77844a5bcfaca5be2a3d05cdb5eae8abdd8
Pavel Bobkov <microsmaster@rambler.ru>
2017-05-18 23:13:31 +0300

    add models for view layer

---
+++ app/src/main/java/ru/chipenable/bakingapp/model/view/Recipe.java
@@ -0,0 +1,23 @@
+package ru.chipenable.bakingapp.model.view;
+
+public class Recipe {
+
+    private final long id;
+    private final String name;
+    private final String image;
+
+    private Recipe(long id, String name, String image){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+}
---
+++ app/src/main/java/ru/chipenable/bakingapp/model/view/Step.java
@@ -0,0 +1,33 @@
+package ru.chipenable.bakingapp.model.view;
+
+public class Step {
+
+    private final String shortDescription;
+    private final String description;
+    private final String videoUrl;
+    private final String thumbnailUrl;
+
+    public Step(String shortDescription, String description, String videoUrl, String thumbnailUrl){
+        this.shortDescription = shortDescription;
+        this.description = description;
+        this.videoUrl = videoUrl;
+        this.thumbnailUrl = thumbnailUrl; 
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+}
