5c9f2fe4bda70d871ff8e0cdf5e83f340f245cf2
Pavel Bobkov <microsmaster@rambler.ru>
2017-05-29 00:24:34 +0300

    adaptation to tablets

--- app/src/main/java/ru/chipenable/bakingapp/ui/activity/MainActivity.java
+++ app/src/main/java/ru/chipenable/bakingapp/ui/activity/MainActivity.java
@@ -19,17 +19,21 @@ public class MainActivity extends AppCompatActivity implements INavigator {
 
     @Inject Router router;
 
+    private int mainContainerId;
+    private int detailcontainerId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((BakingApp)getApplication()).getAppComponent().inject(this);
+        checkConfiguration();
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment f = fm.findFragmentById(R.id.master_container);
         if (f == null) {
             fm.beginTransaction()
-                    .add(R.id.master_container, new RecipeFragment())
+                    .add(mainContainerId, new RecipeFragment())
                     .commit();
         }
     }
@@ -44,9 +48,16 @@ public class MainActivity extends AppCompatActivity implements INavigator {
     public void handleCommand(Command command) {
         if (command == Command.SHOW_DETAILS){
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.master_container, new RecipeDetailsFragment())
+                    .replace(detailcontainerId, new RecipeDetailsFragment())
                     .addToBackStack(null)
                     .commit();
         }
     }
+
+    private void checkConfiguration(){
+        mainContainerId = R.id.master_container;
+        boolean isTablet = getResources().getBoolean(R.bool.is_tablet);
+        detailcontainerId = isTablet? R.id.detail_container:R.id.master_container;
+    }
+
 }
